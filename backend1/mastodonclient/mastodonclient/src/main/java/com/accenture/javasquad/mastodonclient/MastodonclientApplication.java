b085ea0363bad752090d9c0de0573c1a221dbb7c
Sarah <137887746+s-accenture@users.noreply.github.com>
2023-08-22 14:44:21 +0200

    initial commit

---
+++ backend1/mastodonclient/mastodonclient/src/main/java/com/accenture/javasquad/mastodonclient/MastodonclientApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.javasquad.mastodonclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastodonclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastodonclientApplication.class, args);
+	}
+
+}
