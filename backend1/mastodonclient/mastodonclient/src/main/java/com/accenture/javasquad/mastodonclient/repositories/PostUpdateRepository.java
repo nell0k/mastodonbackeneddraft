11caa2d8bd6c71bb2c527e5afcfc2a516b31f576
nell0k <90093006+nell0k@users.noreply.github.com>
2023-08-22 17:52:35 +0200

    Revert "setting up entities and repositories, need to be adjusted"

--- backend1/mastodonclient/mastodonclient/src/main/java/com/accenture/javasquad/mastodonclient/repositories/PostUpdateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.accenture.javasquad.mastodonclient.repositories;
-
-import com.accenture.javasquad.mastodonclient.entities.Post;
-import com.accenture.javasquad.mastodonclient.entities.PostUpdate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostUpdateRepository extends JpaRepository<PostUpdate, Post> {
-}
--- backend1/mastodonclient/mastodonclient/src/main/java/com/accenture/javasquad/mastodonclient/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.accenture.javasquad.mastodonclient.repositories;
-
-import com.accenture.javasquad.mastodonclient.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
